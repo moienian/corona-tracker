8d5537c450576e5850336a7576ca757b14657b9c
Mohammad Moien <mnmoienian@gmail.com>
2020-09-12 11:36:51 +0430

    add chart for showing data

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchDailyData } from "../../api";
+import { Line } from "react-chartjs-2";
+import styles from "./Chart.module.css";
 
 export default function Chart() {
-  return <div>Chart</div>;
+  const [dailyData, setDailyData] = useState([]);
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+    fetchAPI();
+  });
+
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgba(255, 0, 0, 0.5",
+            fill: true,
+          },
+        ],
+      }}
+    ></Line>
+  ) : null;
+  return <div className={styles.container}>{lineChart}</div>;
 }
